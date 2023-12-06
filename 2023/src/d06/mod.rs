a2b2fe4b516320bfeeed9b902e5a5facbc6a2bde
c <corne.spek15@gmail.com>
2023-12-06 16:28:36 +0100

    day 06 part 2

--- 2023/src/d06/mod.rs
+++ 2023/src/d06/mod.rs
@@ -21,7 +21,46 @@ fn get_races(lines: &Vec<String>) -> Vec<(i32, i32)> {
     races
 }
 
-fn p2(lines: Vec<String>) {}
+fn get_race(lines: &Vec<String>) -> (i64, i64) {
+    let races = get_races(lines);
+
+    races.iter().fold((0, 0), |mut acc, x| {
+        acc.0 = format!("{}{}", acc.0.to_string(), x.0.to_string())
+            .parse::<i64>()
+            .unwrap();
+        acc.1 = format!("{}{}", acc.1.to_string(), x.1.to_string())
+            .parse::<i64>()
+            .unwrap();
+        acc
+    })
+}
+
+fn p2(lines: Vec<String>) {
+    let race = get_race(&lines);
+    let mut sum = -1;
+
+    let mut ways_to_beat = 0;
+
+    for i in 0..=(race.0 / 2) {
+        let dist = (race.0 - i) * i;
+
+        if dist > race.1 {
+            ways_to_beat += 1;
+        }
+    }
+
+    ways_to_beat *= 2;
+    if race.0 % 2 == 0 {
+        ways_to_beat -= 1;
+    }
+
+    match sum {
+        -1 => sum = ways_to_beat,
+        _ => sum *= ways_to_beat,
+    }
+
+    println!("Part 2: {}", sum);
+}
 
 fn p1(lines: Vec<String>) {
     let races = get_races(&lines);
@@ -55,6 +94,6 @@ fn p1(lines: Vec<String>) {
 pub fn d06() {
     let input_file = Path::new("src/d06/in.txt");
 
-    p1(get_lines(input_file));
-    // p2(read_lines(input_file).expect("Couldnt read lines of file"));
+    // p1(get_lines(input_file));
+    p2(get_lines(input_file));
 }
