6e14af028f521ecffc109dd4419566079cda5454
c <corne.spek15@gmail.com>
2023-12-08 20:07:26 +0100

    day 08 part 2

--- 2023/src/d08/mod.rs
+++ 2023/src/d08/mod.rs
@@ -7,6 +7,9 @@ use crate::util::get_lines;
 struct Graph {
     nodes: IndexMap<String, (String, String)>,
     instr: Vec<char>,
+
+    // part 2 only
+    start_nodes: Vec<String>,
 }
 
 impl Graph {
@@ -17,6 +20,8 @@ impl Graph {
         }
 
         let mut nodes: IndexMap<String, (String, String)> = IndexMap::new();
+        let mut start_nodes: Vec<String> = vec![];
+
         for line in lines.iter().skip(2) {
             let words: Vec<&str> = line.split_whitespace().collect();
 
@@ -27,14 +32,69 @@ impl Graph {
             let mut r = words[3].to_string();
             r.remove(l.len());
 
+            let last_char = words[0].chars().last().unwrap();
+            match last_char {
+                'A' => start_nodes.push(words[0].to_string()),
+                _ => (),
+            }
+
             nodes.insert(words[0].to_string(), (l, r));
         }
 
-        Self { nodes, instr }
+        Self {
+            nodes,
+            instr,
+            start_nodes,
+        }
     }
 }
 
-fn p2(lines: Vec<String>) {}
+fn p2(lines: Vec<String>) {
+    let graph = Graph::from_lines(&lines);
+
+    let mut n_steps: Vec<i64> = vec![];
+
+    for start_node in &graph.start_nodes {
+        let mut c = 0;
+        let mut curr = start_node;
+
+        while curr.chars().last().unwrap() != 'Z' {
+            for dir in &graph.instr {
+                let next = &graph.nodes.get(curr).unwrap();
+                if *dir == 'L' {
+                    curr = &next.0;
+                } else {
+                    curr = &next.1;
+                }
+
+                c += 1;
+
+                if curr.chars().last().unwrap() == 'Z' {
+                    break;
+                }
+            }
+        }
+
+        n_steps.push(c);
+    }
+
+    fn gcd(a: i64, b: i64) -> i64 {
+        match b {
+            0 => a,
+            _ => gcd(b, a % b),
+        }
+    }
+
+    fn lcm(a: i64, b: i64) -> i64 {
+        a / gcd(a, b) * b
+    }
+
+    fn lcmm(nums: &Vec<i64>) -> i64 {
+        nums.iter().fold(1, |acc, &e| lcm(acc, e))
+    }
+
+    println!("Part 2: {}", lcmm(&n_steps));
+}
 
 fn p1(lines: Vec<String>) {
     let graph = Graph::from_lines(&lines);
@@ -65,6 +125,6 @@ fn p1(lines: Vec<String>) {
 pub fn d08() {
     let input_file = Path::new("src/d08/in.txt");
 
-    p1(get_lines(input_file));
-    // p2(get_lines(input_file));
+    // p1(get_lines(input_file));
+    p2(get_lines(input_file));
 }
